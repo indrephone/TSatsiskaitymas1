d8d42553f7a71681915f772a1adeec9f5ddd2f59
indrephone <indrephone@gmail.com>
2024-07-19 09:44:29 +0300

    Antra Uzduotis

---
+++ Task 2/script.js
@@ -0,0 +1,38 @@
+/* ------------------------------ TASK 2 -----------------------------------
+Aprašykite type'us, kad kintamieji, kuriems jie priskirti būtų teisingai aprašyti ir visas kodas veiktų teisingai.
+Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.-------------------------------------------------------------------------- */
+// panaudojam
+var one2 = {
+    vardas: '',
+    pavarde: '',
+    amzius: 0
+};
+// panaudojam
+var two2 = {
+    vardas: '',
+    pavarde: '',
+    amzius: 0
+};
+two2 = {
+    vardas: '',
+    pavarde: '',
+    amzius: 0,
+    ugis: 0,
+    svoris: 0
+};
+// panaudojam
+var three = {
+    vardas: '',
+    pavarde: '',
+    lytis: 'vyras'
+};
+three = {
+    vardas: '',
+    pavarde: '',
+    lytis: 'moteris'
+};
+three = {
+    vardas: '',
+    pavarde: '',
+    // lytis: 'sraigtasparnis' // error
+};
--- Task 2/script.ts
+++ Task 2/script.ts
@@ -1,32 +1,53 @@
 /* ------------------------------ TASK 2 -----------------------------------
 Aprašykite type'us, kad kintamieji, kuriems jie priskirti būtų teisingai aprašyti ir visas kodas veiktų teisingai.
 Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.-------------------------------------------------------------------------- */
+
+// sukuriam tipa
 type One = {
-
+  vardas: string,
+  pavarde: string,
+  amzius: number
 }
-let one: One = {
+// panaudojam
+let one2: One = {
   vardas: '',
   pavarde: '',
   amzius: 0
 };
+
+// sukuriam tipa
 type Two = {
-
+   vardas: string,
+   pavarde: string,
+   amzius: number,
+   ugis?: number,
+   svoris?:number
 }
-let two: Two = {
+
+// panaudojam
+let two2: Two = {
   vardas: '',
   pavarde: '',
   amzius: 0
 };
-two = {
+two2 = {
   vardas: '',
   pavarde: '',
   amzius: 0,
   ugis: 0,
   svoris: 0
 };
-type Three = {
 
+
+
+// sukuriam tipa
+type Three = {
+  vardas: string,
+  pavarde: string,
+  lytis?: 'vyras' | 'moteris'
 }
+
+// panaudojam
 let three: Three = {
   vardas: '',
   pavarde: '',
@@ -40,5 +61,5 @@ three = {
 three = {
   vardas: '',
   pavarde: '',
-  lytis: 'sraigtasparnis' // error
+  // lytis: 'sraigtasparnis' // error
 };
